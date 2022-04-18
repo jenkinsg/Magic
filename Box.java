086a8d44ec6e5f61d7bdf7d6bcb89df9943da06c
jenkinsg <103235835+jenkinsg@users.noreply.github.com>
2022-04-18 08:43:29 +0530

    Update Box.java

--- Box.java
+++ Box.java
@@ -2,7 +2,8 @@ class Box {
 public static void main(String args[]) {
  System.out.println("Sample");
  System.out.println("* * * * *");
- System.out.println("Jenkins");
+ System.out.println(" Forking Repository From PTIS-1 To Jenkinsg ");
  System.out.println("* * * * *");
+ System.out.println("-------");
 }
 }
