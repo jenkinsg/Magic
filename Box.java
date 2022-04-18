da3479640dcc3578ec1c3c27c17c47898ec5db82
githubjenkin <githubjenkins409@gmail.com>
2022-04-18 08:23:27 +0530

    first commit

---
+++ Box.java
@@ -0,0 +1,8 @@
+class Box {
+public static void main(String args[]) {
+ System.out.println("Sample");
+ System.out.println("* * * * *");
+ System.out.println("Jenkins");
+ System.out.println("* * * * *");
+}
+}
